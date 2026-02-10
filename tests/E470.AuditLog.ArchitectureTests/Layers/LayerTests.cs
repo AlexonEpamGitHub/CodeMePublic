148e370ba0810a9b2f3bf86d8b7e0b88d42e1efe
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2026-02-10 20:35:03 +0200

    Create E470.AuditLog.ArchitectureTests LayerTests.cs

---
+++ tests/E470.AuditLog.ArchitectureTests/Layers/LayerTests.cs
@@ -0,0 +1,74 @@
+using NetArchTest.Rules;
+using Shouldly;
+
+namespace E470.AuditLog.ArchitectureTests.Layers;
+
+[TestFixture]
+public class LayerTests : BaseTest
+{
+    [Test]
+    public void Domain_Should_NotHaveDependencyOnApplication()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn("E470.AuditLog.Application")
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Test]
+    public void DomainLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Test]
+    public void DomainLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Test]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        TestResult result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Test]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        TestResult result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Test]
+    public void InfrastructureLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        TestResult result = Types.InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+}
