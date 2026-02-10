e91052ffa87ef51aa94f835523d7743639cfd72b
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2026-02-10 20:05:47 +0200

    Create E470.AuditLog.SharedKernel - Error.cs

---
+++ audit-log/src/E470.AuditLog.SharedKernel/Error.cs
@@ -0,0 +1,18 @@
+namespace E470.AuditLog.SharedKernel;
+
+public sealed record Error(string Code, string Description, ErrorType Type)
+{
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+
+    public static Error Failure(string code, string description) =>
+        new(code, description, ErrorType.Failure);
+
+    public static Error NotFound(string code, string description) =>
+        new(code, description, ErrorType.NotFound);
+
+    public static Error Problem(string code, string description) =>
+        new(code, description, ErrorType.Problem);
+
+    public static Error Conflict(string code, string description) =>
+        new(code, description, ErrorType.Conflict);
+}
